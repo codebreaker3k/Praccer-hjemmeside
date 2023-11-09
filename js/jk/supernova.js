e18c26873a95f66568b0426db530a4f1c72f64e7
Roy TS <royterjestrommen@hotmail.com>
2023-11-09 19:00:30 +0100

    *

---
+++ js/jk/supernova.js
@@ -0,0 +1,99 @@
+console.log ("Halla Joakim")
+
+let teamArray = ["MUTD, Liverpool, Swansea, MCITY, Arsenal, Tottenham, Wolves, NewCastle, Brentford, Fulham"]
+console.log(teamArray)
+
+teamArray.pop()
+console.log (teamArray)
+
+teamArray.push ("Møkkalag1, Møkkalag2")
+console.log(teamArray)
+
+// teamArray= ArrayUtils.removeElement(teamArray[1])
+// gi meg gjerne en linje og eksempel på å fjerne spesifikk array
+
+const team = [{
+teamName:" Tottenham"
+}]
+console.log(team)
+
+
+const MUTD = team[1];
+const Liverpool = team[2];
+const Swansea = team [3];
+const Arsenal = team[4];
+const Tottenham = team[5];
+
+console.log(teamArray[5]);"Dette er index 5"
+
+
+let Topp2Array = ["MEG og DEG"]
+console.log(Topp2Array)
+
+const variabel3 = "Mot Verden"
+console.log(`${Topp2Array} ${variabel3}`)
+
+const noyski = personer[1]
+const Tullegutten = personer[2]
+
+const personer = [
+{
+    Fornavn: "noyski",
+Etternavn: "oyski",
+Alder: 300,
+},
+{
+    Fornavn: "Tullegutten",
+    Etternavn: "Tei",
+    Alder: 337  
+}
+];
+
+
+console.log
+
+const poyskiAlder = noyski.Alder
+const poyskiEtternavn = oyski.Etternavn
+const TulleguttenEtternavn = jättegammel.Etternavn
+const TulleguttenAlder = Tullegutten.Alder
+
+console.log (personer[1].Fornavn + personer.Etternavn, + personer.Alder);
+console.log(personer[2].Fornavn, + personer.Etternavn, + personer.Alder);
+
+
+simpleFunction()
+
+function getRandomNumber(maxnumber){
+    return Math.round(Math.random() * 5)
+}
+
+getRandomNumber(1-5 + 1)
+
+console.log(getRandomNumber)
+
+function giveCompliment(){
+    const compliments = [
+        "You're a star",
+        "THE BEST",
+        "HAHA YOUR FUN",
+    ]
+
+const randomIndex = Math.round(Math.random() * (compliments.length - 1));
+const compliment = compliments[randomIndex]
+return compliment 
+}
+console.log(giveCompliment())
+
+/*Lag en logikk for en bedrift slik som vi har øvd på tidligere. LAG EN NY logikk, ikke bruk samme dere jobbet med på torsdag.
+Det skal være med if og else
+BONUS: Bruk if else minst 2 ganger
+console.log ut resultatet.*/
+
+/* IndexOff for å fjerne noe spesifikt ut fra et array*/
+
+/* ' Single-line syntax: -- "untermined string literal - hvorfor? */
+
+if 
+    (duherjer);
+
+else; (hjem)
